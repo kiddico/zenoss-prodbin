5517a183317342820a7ccc275182f0ace3558a3d
Chet Luther <chet.luther@gmail.com>
2020-06-23 16:46:52 -0400

    send device organizers to cloud
    This was done for the Summer 2019 Hackathon Map Tile project. We needed
    to get locations send to cloud with their address and latlong
    properties.

--- Products/Zing/model_updates.py
+++ Products/Zing/model_updates.py
@@ -16,6 +16,7 @@ from zope.interface import implements
 
 from Products.ZenModel.Device import Device
 from Products.ZenModel.DeviceComponent import DeviceComponent
+from Products.ZenModel.DeviceOrganizer import DeviceOrganizer
 from Products.Zing import fact as ZFact
 from Products.Zing.interfaces import IZingObjectUpdateHandler
 from Products.Zing.tx_state import ZingTxStateManager
@@ -38,31 +39,41 @@ class ZingObjectUpdateHandler(object):
             uuid = obj.getUUID()
         except Exception:
             pass
-        return uuid and (isinstance(obj, Device) or isinstance(obj, DeviceComponent))
+        return uuid and (
+            isinstance(obj, Device) or
+            isinstance(obj, DeviceComponent) or
+            isinstance(obj, DeviceOrganizer))
 
     def _get_zing_tx_state(self):
         """ """
         return self.zing_tx_state_manager.get_zing_tx_state(self.context)
 
     def _update_object(self, obj, idxs=None):
-        if self.is_object_relevant(obj) and isinstance(obj, Device):
-            tx_state = self._get_zing_tx_state()
-            uuid = obj.getUUID()
-            tx_state.need_deletion_fact.pop(uuid, None)
-            if idxs:
-                # set this to debug
-                log.debug("buffering object update for {}".format(uuid))
-                if "path" in idxs:
-                    device_fact = ZFact.organizer_fact_from_device(obj)
-                    tx_state.need_organizers_fact[uuid] = device_fact
-                    # we also need to generate organizers facts for all the device components
-                    for comp_brain in obj.componentSearch(query={}):
-                        if not comp_brain.getUUID:
-                            continue
-                        comp_fact = ZFact.organizer_fact_from_device_component(device_fact, comp_brain.getUUID, comp_brain.meta_type)
-                        tx_state.need_organizers_fact[comp_brain.getUUID] = comp_fact
-                if "name" in idxs or "productionState" in idxs:
-                    tx_state.need_device_info_fact[uuid] = ZFact.device_info_fact(obj)
+        if not self.is_object_relevant(obj):
+            return
+
+        tx_state = self._get_zing_tx_state()
+        uuid = obj.getUUID()
+        tx_state.need_deletion_fact.pop(uuid, None)
+
+        if isinstance(obj, Device) and idxs:
+            # set this to debug
+            log.debug("buffering object update for {}".format(uuid))
+            if "path" in idxs:
+                device_fact = ZFact.organizer_fact_from_device(obj)
+                tx_state.need_organizers_fact[uuid] = device_fact
+                # we also need to generate organizers facts for all the device components
+                for comp_brain in obj.componentSearch(query={}):
+                    if not comp_brain.getUUID:
+                        continue
+                    comp_fact = ZFact.organizer_fact_from_device_component(device_fact, comp_brain.getUUID, comp_brain.meta_type)
+                    tx_state.need_organizers_fact[comp_brain.getUUID] = comp_fact
+            if "name" in idxs or "productionState" in idxs:
+                tx_state.need_device_info_fact[uuid] = ZFact.device_info_fact(obj)
+
+        if isinstance(obj, DeviceOrganizer):
+            fact = ZFact.device_organizer_info_fact(obj)
+            tx_state.need_device_organizer_info_fact[uuid] = fact
 
     def _delete_object(self, obj):
         if self.is_object_relevant(obj):
@@ -113,6 +124,11 @@ class ZingObjectUpdateHandler(object):
             log.info("Processing {} device info updates".format(len(tx_state.need_device_info_fact)))
             fact_generators.append(self._generate_facts(tx_state.need_device_info_fact,
                                    tx_state.already_generated_device_info_facts))
+        if tx_state.need_device_organizer_info_fact:
+            # TODO set this to debug
+            log.info("Processing {} device organizer info updates".format(len(tx_state.need_device_organizer_info_fact)))
+            fact_generators.append(self._generate_facts(tx_state.need_device_organizer_info_fact,
+                                                        tx_state.already_generated_device_organizer_info_facts))
         if tx_state.need_organizers_fact:
             # TODO set this to debug
             log.info("Processing {} organizers updates".format(len(tx_state.need_organizers_fact)))
